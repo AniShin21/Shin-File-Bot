29be0c7173ba8914e8e11575d1ee42eaaef3b515
AniShin <minecraft2727k@gmail.com>
2024-08-21 20:45:23 +0530

    Create cbb.py

---
+++ plugins/cbb.py
@@ -0,0 +1,27 @@
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text=f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Owner : <a href='tg://user?id={OWNER_ID}'>Him</a>\n┣⪼ Source : <a href'https://t.me/ikigai_bots'>IKigai</a>\n┣⪼ Language : <a href='https://www.python.org/'>Python</a>\n┣⪼ Library :<a href='https://docs.pyrogram.org/'>Pyrogram</a>\n┣⪼ Hosting :<a href='https://www.heroku.com/'>Heroku</a>\n╰───────────────⍟</b",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data="close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
