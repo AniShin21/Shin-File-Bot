78b6586a011ff61609988345490f3cadfdb04208
AniShin <minecraft2727k@gmail.com>
2024-08-23 07:04:48 +0530

    Create premium.py

---
+++ plugins/premium.py
@@ -0,0 +1,13 @@
+"""
+
+Bruh! Still in hope of finding repo with spicial features
+I am sorry i can't provide script for free 
+You Can buy It
+if you want to buy then contact me on telegram
+( iTz_Anayokoji )
+
+This script is spicifed for Token verifaction 
+if you enable it then you can use this 
+else there is no use of this script 
+
+"""
