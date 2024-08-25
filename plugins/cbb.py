c1e732aaca049ca0a8708d59224ffba5e5d850b8
AniShin <minecraft2727k@gmail.com>
2024-08-25 12:34:15 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,7 +9,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Owner : <a href='tg://user?id={OWNER_ID}'>Him</a>\n┣⪼ Source : <a href'https://github.com/AniShin21/Shin-File-Bot.git'>Click Here</a>\n┣⪼ Language : <a href='https://www.python.org/'>Python</a>\n┣⪼ Library :<a href='https://docs.pyrogram.org/'>Pyrogram</a>\n┣⪼ Hosting :<a href='https://www.heroku.com/'>Heroku</a>\n╰───────────────⍟</b",
+            text=f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Owner : <a href='tg://user?id={OWNER_ID}'>Him</a>\n┣⪼ Source : <a href'https://github.com/AniShin21/Shin-File-Bot.git'>Click Here</a>\n┣⪼ Language : <a href='https://www.python.org/'>Python</a>\n┣⪼ Library :<a href='https://docs.pyrogram.org/'>Pyrogram</a>\n┣⪼ Hosting :<a href='https://www.heroku.com/'>Heroku</a>\n╰───────────────⍟</b>",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
@@ -33,7 +33,24 @@ elif data == "prenium":
     await query.message.edit_text(
         text=f"""
         Yo {first}{last}
-        This Premium Feature Is About GitHub Repo
-        °
+        This Premium Feature Is About GitHub Repo--
+        
+        °You Will Get Token Verifaction On/Off
+        
+        °Add Premium User To Bot If Token Verifaction Is On
+         Bot Will Not Let Premium User To Verify And Skip It
+         
+        °You Can Add Admins From Bot
+        
+        °If You Have Anime Bot Then You Will Get a Spicial Feature 
+         /top, /weekly, /search <Anime Name> With These Commands
+         """),
+reply_markup=InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("Return", callback_data="about")
+        ]
+    ]
+)
 
         
